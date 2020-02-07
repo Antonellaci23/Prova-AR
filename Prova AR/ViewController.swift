361ff2ed5dc9c13602c0f9bd7c27da8888aecd6b
Antonella <antonellacirma@gmail.com>
2020-02-07 12:19:31 +0100

    AR Kit

--- Prova AR/ViewController.swift
+++ Prova AR/ViewController.swift
@@ -11,65 +11,78 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    var heartNode: SCNNode?
+    var diamondNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+        let heartScene = SCNScene(named: "art.scnassets/heart.scn")
+        let diamondScene = SCNScene(named: "art.scnassets/diamond.scn")
+        heartNode = heartScene?.rootNode
+        diamondNode = diamondScene?.rootNode
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "photos", bundle: Bundle.main) {
+            configuration.detectionImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 2
+            
+        }
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchoe = anchor as? ARImageAnchor {
+            
+            let size = imageAnchoe.referenceImage.physicalSize
+            let plane = SCNPlane (width: size.width, height: size.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            plane.cornerRadius = 0.005
+            let planeNode = SCNNode (geometry: plane)
+            planeNode.eulerAngles.x = -.pi/2
+            node.addChildNode (planeNode)
+            
+            var shapeNode: SCNNode?
+            switch imageAnchoe.referenceImage.name! {
+            case camera.badge.rawValue:
+                shapeNode = heartNode
+            case camera.book.rawValue:
+                shapeNode = diamondNode
+            default:
+                break
+            }
+            
+            
+            guard let shape = shapeNode else {
+                
+                return nil
+                
+            }
+            node.addChildNode (shape)
+        }
+        
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    enum camera: String {
+        case badge = "Badge"
+        case book = "Book"
     }
 }
+
